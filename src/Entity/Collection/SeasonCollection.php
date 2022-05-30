19e45d2d3fb9143e2136160846d01936d1580a54
Gabriel Houdry--Bohême <gabriel.houdry--boheme@etudiant.univ-reims.fr>
2022-05-30 17:20:24 +0200

    Classe SeasonCollection.php créée

---
+++ src/Entity/Collection/SeasonCollection.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Entity\Collection;
+
+use Entity\Season;
+use Database\MyPdo;
+use PDO;
+
+class SeasonCollection
+{
+    public function findByTVShowId(int $tvShowId): array
+    {
+        $stmt = MyPDO::getInstance()->prepare(
+            <<<'SQL'
+            SELECT id, tvShowId, name, seasonNumber, posterId
+            FROM season
+            WHERE tvShowId = :id
+            ORDER BY seasonNumber
+        SQL
+        );
+        $stmt->bindParam(':id', $tvShowId);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, 'Entity\Season');
+    }
+}
