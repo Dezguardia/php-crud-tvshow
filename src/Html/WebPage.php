66fe71fa22ac0d028756bfb2518ce1617b67636f
Elie Pouget <elie.pouget@etudiant.univ-reims.fr>
2022-05-31 11:09:41 +0200

    Ajout des getteurs à la class WebPage

--- src/Html/WebPage.php
+++ src/Html/WebPage.php
@@ -4,5 +4,44 @@ namespace Elie\PhpCrudTvshow\Html;
 
 class WebPage
 {
+    private string $head;
+    private string $title;
+    private string $body;
+
+    /**
+     * WebPage constructor.
+     * @param string $title Titre de la page
+     */
+    public function __construct(string $title = "")
+    {
+        $this->title = $title;
+        $this->head = "";
+        $this->body = "";
+    }
+
+    /**
+     * @return string
+     */
+    public function getHead(): string
+    {
+        return $this->head;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBody(): string
+    {
+        return $this->body;
+    }
+
 
 }
