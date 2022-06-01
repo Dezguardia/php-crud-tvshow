9784134f44eee18c34e3d05135421ebfc11b0090
Elie Pouget <elie.pouget@etudiant.univ-reims.fr>
2022-06-01 10:23:25 +0200

    Ajout de la méthoode findBySeasonId à la class EpisodeCollection

--- src/Entity/Collection/EpisodeCollection.php
+++ src/Entity/Collection/EpisodeCollection.php
@@ -8,5 +8,23 @@ use Entity\Episode;
 
 class EpisodeCollection
 {
-
+    /**Search a database collection for a document or set of documents.
+     * The found documents are returned as a CollectionFind object is
+     * to further modify or fetch results from.
+     * @return Episode[]
+     */
+    public static function findBySeasonId(int $saisonId): array
+    {
+        $episode = MyPDO::getInstance()->prepare(
+            <<<'SQL'
+            SELECT id, seasonId, name, overview, episodeNumber
+            FROM episode
+            WHERE seasonId = :id
+            ORDER BY episodeNumber;
+        SQL
+        );
+        $episode->bindParam(':id', $saisonId);
+        $episode->execute();
+        return $episode->fetchAll(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, "Entity\Episode");
+    }
 }
