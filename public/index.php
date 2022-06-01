53aea4394ac8e5f67bb6997d34b85f93e697842e
Elie Pouget <elie.pouget@etudiant.univ-reims.fr>
2022-06-01 12:51:53 +0200

    Modifcation des class + ajout des images

--- public/index.php
+++ public/index.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 require_once '../vendor/autoload.php';
 
@@ -14,19 +15,25 @@ $page->appendCssUrl('css\style.css');
 
 $stmt = new TvShowCollection();
 $stmt = $stmt->findAll();
-
+$res = 0;
 $page->appendContent("<h1>Séries TV</h1><div class=list>");
 foreach ($stmt as $ligne) {
+    $res += 1;
+    if (($res%2) == 1) {
+        $div = "gauche";
+    } else {
+        $div = "droite";
+    }
     $nom = WebPage::escapeString($ligne->getName());
     $tvShowId = WebPage::escapeString((string)$ligne->getId());
     $overview = WebPage::escapeString($ligne->getOverview());
     $posterId = strval($ligne->getPosterId());
     $page->appendContent(<<<HTML
-        <div class='serie'><a href="tvshow.php?tvShowId=$tvShowId"> \n
-            <img src='poster.php?posterId=$posterId' class="index_poster">\n
-            <h2>{$nom}</h2>\n
-            <p>{$overview}</p>\n
-        </a></div>\n
+        <a href="tvshow.php?tvShowId=$tvShowId" class="serie">
+            <img src='poster.php?posterId=$posterId' class="$div">
+            <h3>$nom</h3>
+            <p>$overview</p>
+        </a>
     HTML);
 }
 $page->appendContent('</div>');
