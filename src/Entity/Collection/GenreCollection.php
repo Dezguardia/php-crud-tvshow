f48f394069ae168884fdab44472912282a850dba
Elie Pouget <elie.pouget@etudiant.univ-reims.fr>
2022-06-01 21:08:46 +0200

    Suppression d'éléments en trop + ajout de la méthode findByGenreName dans TvShowCollection.php

--- src/Entity/Collection/GenreCollection.php
+++ src/Entity/Collection/GenreCollection.php
@@ -13,25 +13,6 @@ class GenreCollection
      * to further modify or fetch results from.
      * @return Genre[]
      */
-    public static function findByTvShowGenreId(int $tvShowGenreId): array
-    {
-        $genre = MyPDO::getInstance()->prepare(
-            <<<'SQL'
-            SELECT id, name
-            FROM genre
-            WHERE genreId = :id
-            ORDER BY id;
-        SQL
-        );
-        $genre->bindParam(':id', $tvShowGenreId);
-        $genre->execute();
-        return $genre->fetchAll(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, "Entity\Genre");
-    }
-    /**Search a database collection for a document or set of documents.
-     * The found documents are returned as a CollectionFind object is
-     * to further modify or fetch results from.
-     * @return Genre[]
-     */
     public static function findAll(): array
     {
         $stmt = MyPDO::getInstance()->prepare(
--- src/Entity/Collection/TvShowCollection.php
+++ src/Entity/Collection/TvShowCollection.php
@@ -26,4 +26,25 @@ class TvShowCollection
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, "Entity\TvShow");
     }
+    /**Search a database collection for a document or set of documents.
+     * The found documents are returned as a CollectionFind object is
+     * to further modify or fetch results from.
+     * @return TvShow[]
+     */
+    public static function findByGenreName(string $genreName): array
+    {
+        $stmt = MyPDO::getInstance()->prepare(
+            <<<'SQL'
+               SELECT t.id, t.name, t.originalName, t.homepage, t.overview, t.posterId
+            FROM tvshow t 
+                INNER JOIN tvshow_genre tg ON (t.id=tg.tvShowId)
+                INNER JOIN genre g ON (g.id=tg.genreId)
+            WHERE g.name = :name
+            ORDER BY name
+        SQL
+        );
+        $stmt->bindParam(':name', $genreName);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, "Entity\TvShow");
+    }
 }
