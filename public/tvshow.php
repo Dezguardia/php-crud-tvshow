782973a6f060d7318d9e4d35ce522085844356c7
Gabriel Houdry--Bohême <gabriel.houdry--boheme@etudiant.univ-reims.fr>
2022-06-01 12:50:20 +0200

    Correction de tvshow.php

--- public/tvshow.php
+++ public/tvshow.php
@@ -38,12 +38,13 @@ $tvshowPoster = $tvshow->getPosterId();
 
 $tvshowpage->appendContent(<<<HTML
             <h1>Séries TV : {$tvshow->getName()}</h1>\n
-            <div class = 'tvshow_info'>\n
-                <div class = 'tvshow_info_name'>{$showname}</div>\n
-                <div class = 'tvshow_poster'><img src="poster.php?posterId=$tvshowPoster"></div>
-                <div class = 'tvshow_info_originalname'>{$showOriginalName}</div>\n
-                <div class = 'tvshow_info_overview'>{$showOverview}</div>\n
-            </div>\n
+            <div class="list">\n
+                <div class = 'tvshow_info'>\n
+                    <div class = 'tvshow_info_name'>{$showname}</div>\n
+                    <div class = 'tvshow_poster'><img src="poster.php?posterId=$tvshowPoster"></div>
+                    <div class = 'tvshow_info_originalname'>{$showOriginalName}</div>\n
+                    <div class = 'tvshow_info_overview'>{$showOverview}</div>\n
+                </div>\n
     HTML);
 
 for ($i=0;$i<count($stmt);$i++) {
@@ -51,11 +52,13 @@ for ($i=0;$i<count($stmt);$i++) {
     $seasonId = WebPage::escapeString((string)$stmt[$i]->getId());
     $poster = strval($stmt[$i]->getPosterId());
     $tvshowpage->appendContent(<<<HTML
-        <div class='season'><a href="season.php?tvShowId=$tvshowId&seasonId=$seasonId">\n
-            <div class ='season_poster'><img src="poster.php?posterId=$poster" alt=""></div>
-            <div class='season_name'><p>$name</p></div>\n
-        </a></div>\n
+                <div class='season'><a href="season.php?tvShowId=$tvshowId&seasonId=$seasonId">\n
+                    <div class ='season_poster'><img src="poster.php?posterId=$poster" alt=""></div>
+                    <div class='season_name'><p>$name</p></div>\n
+                </a></div>\n
     HTML);
 }
-
+$tvshowpage->appendContent(<<<HTML
+        </div>\n
+HTML);
 echo $tvshowpage->toHTML();
