a911948a4c1ae21dd0803ee83b99220f643dd619
Elie Pouget <elie.pouget@etudiant.univ-reims.fr>
2022-06-01 18:39:14 +0200

    Ajout des getters de la class TvShowGenre.php

--- src/Entity/TvShowGenre.php
+++ src/Entity/TvShowGenre.php
@@ -4,5 +4,31 @@ namespace Entity;
 
 class TvShowGenre
 {
+    private int $id;
+    private int $genreId;
+    private int $tvShowId;
 
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getGenreId(): int
+    {
+        return $this->genreId;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTvShowId(): int
+    {
+        return $this->tvShowId;
+    }
 }
