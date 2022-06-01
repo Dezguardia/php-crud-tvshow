1188abb6ebde74d3914f58f31c88b861eee883f6
Elie Pouget <elie.pouget@etudiant.univ-reims.fr>
2022-06-01 18:42:57 +0200

    Ajout des getteurs de la class genre.php

--- src/Entity/Genre.php
+++ src/Entity/Genre.php
@@ -6,4 +6,22 @@ class Genre
 {
     private int $id;
     private string $name;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+
 }
