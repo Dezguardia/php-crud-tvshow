a244ed7ceffed998c90e5e9fa586839ff0e8151d
Elie Pouget <elie.pouget@etudiant.univ-reims.fr>
2022-06-01 08:45:57 +0200

    Ajout des déclaration de la class Episode

--- src/Entity/Episode.php
+++ src/Entity/Episode.php
@@ -4,5 +4,9 @@ namespace Entity;
 
 class Episode
 {
-
+    private int $id;
+    private int $seasonId;
+    private string $name;
+    private string  $overview;
+    private int $episodeNumber;
 }
