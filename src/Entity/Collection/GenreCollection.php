2ed5eca72c8c02e0803108333ad6b0a837e24e17
Elie Pouget <elie.pouget@etudiant.univ-reims.fr>
2022-06-01 19:35:31 +0200

    Ajout des méthodes findAll dans la class GenreCollection.php

--- src/Entity/Collection/GenreCollection.php
+++ src/Entity/Collection/GenreCollection.php
@@ -27,4 +27,21 @@ class GenreCollection
         $genre->execute();
         return $genre->fetchAll(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, "Entity\Genre");
     }
+    /**Search a database collection for a document or set of documents.
+     * The found documents are returned as a CollectionFind object is
+     * to further modify or fetch results from.
+     * @return Genre[]
+     */
+    public static function findAll(): array
+    {
+        $stmt = MyPDO::getInstance()->prepare(
+            <<<'SQL'
+               SELECT id, name
+            FROM genre
+            ORDER BY name
+        SQL
+        );
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, "Entity\Genre");
+    }
 }
