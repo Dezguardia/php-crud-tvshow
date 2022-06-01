1023a5924337c5528fe3617fb186b30020d4b900
Elie Pouget <elie.pouget@etudiant.univ-reims.fr>
2022-06-01 08:46:46 +0200

    Ajout des getteurs de la class Episode

--- src/Entity/Episode.php
+++ src/Entity/Episode.php
@@ -9,4 +9,44 @@ class Episode
     private string $name;
     private string  $overview;
     private int $episodeNumber;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getSeasonId(): int
+    {
+        return $this->seasonId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOverview(): string
+    {
+        return $this->overview;
+    }
+
+    /**
+     * @return int
+     */
+    public function getEpisodeNumber(): int
+    {
+        return $this->episodeNumber;
+    }
 }
